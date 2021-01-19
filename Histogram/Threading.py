9c1336fcd57abb2a21027a1f085ddd4d6535015d
Haliima Ahmed <43786387+HaliimaAhmed@users.noreply.github.com>
2021-01-18 20:37:42 -0500

    Update Threading.py

--- Histogram/Threading.py
+++ Histogram/Threading.py
@@ -5,10 +5,6 @@ from Entity import _Entity
 
 """
 By: Haliima Ahmed
-Course:CST8333 Programming Language Research Project
-Professor Name:Stanley Pieda
-Assignment: 4
-Due: March 25th,2020
 thread class that will load the records inside the dataset.  
 
 """
