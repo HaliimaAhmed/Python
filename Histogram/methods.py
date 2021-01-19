bc0b1fdf1e0525d69ea73e10d75c8699459afd41
Haliima Ahmed <43786387+HaliimaAhmed@users.noreply.github.com>
2021-01-18 20:38:05 -0500

    Update methods.py

--- Histogram/methods.py
+++ Histogram/methods.py
@@ -4,10 +4,6 @@ from Entity import _Entity
 
 """
 By: Haliima Ahmed
-Course:CST8333 Programming Language Research Project
-Professor Name:Stanley Pieda
-Assignment: 4
-Due: March 25th,2020
 This class edits, deletes, writes the datafile from the CSV file  
 """
 
